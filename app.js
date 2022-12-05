76fd414eb2866a77176cdf47ca8863e2f2349402
Mael <contact@maeldev.fr>
2022-12-05 13:58:59 +0100

    V1.0.1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
+const { autoUpdater } = require("electron-updater");
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
@@ -9,6 +10,7 @@ const createWindow = () => {
     title: "MOCMA Notes",
     webPreferences: {
       nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
@@ -16,30 +18,37 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
+  autoUpdater.checkForUpdatesAndNotify();
   createWindow();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
-const menu = Menu.buildFromTemplate([
-  {
-    label: "MOCMA Notes",
-    submenu: [
-      {
-        label: "Quit Free Note",
-        click: () => {
-          app.quit();
-        },
-        accelerator: "CmdOrCtrl+Q",
-      },
-    ],
-  },
-]);
+// const menu = Menu.buildFromTemplate([
+//   {
+//     label: "MOCMA Notes",
+//     submenu: [
+//       {
+//         label: "Quit Free Note",
+//         click: () => {
+//           app.quit();
+//         },
+//         accelerator: "CmdOrCtrl+Q",
+//       },
+//     ],
+//   },
+// ]);
 
-Menu.setApplicationMenu(menu);
+// Menu.setApplicationMenu(menu);
